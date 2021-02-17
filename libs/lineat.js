9530c718e47a998f09df6f360d5bc3ed57a66714
李元琦 <yuan.chi@program.com.tw>
2021-02-17 16:40:01 +0800

    test

--- libs/lineat.js
+++ libs/lineat.js
@@ -2,9 +2,9 @@ const { getenv } = require('../libs/helpers')
 const line = require('@line/bot-sdk')
 
 const config = {
-  channelId: getenv('LINE_CHANNEL_ID'),
-  channelSecret: getenv('LINE_CHANNEL_SECRET'),
-  channelAccessToken: getenv('LINE_CHANNEL_ACCESSTOKEN'),
+  channelId: getenv('DEV_LINE_CHANNEL_ID'),
+  channelSecret: getenv('DEV_LINE_CHANNEL_SECRET'),
+  channelAccessToken: getenv('DEV_LINE_CHANNEL_ACCESSTOKEN'),
 }
 
 const client = new line.Client(config)
